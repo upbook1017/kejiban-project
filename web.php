c5ac2fcede4dcc327c2ec7dfd1adfe33378af677
upbook1017 <164146088+upbook1017@users.noreply.github.com>
2024-04-10 23:11:06 +0900

    2024/4/10作業記録
    cssのmainのリンク位置を調整。

--- web.php
+++ web.php
@@ -3,6 +3,10 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/', function () {
+    return view('welcome');
+});//:8000のlocalhostページのルートコード
+
 /*Route::get('home', function () {
     return view('home.index');
 });*/
