2c90241ae22c1f35b5bf9b84ab8e81b7c79903f3
upbook1017 <164146088+upbook1017@users.noreply.github.com>
2024-04-12 00:41:26 +0900

    2024/4/12作業記録
    cssの設定追加/blade.phpの表を追加

--- index.blade.php
+++ index.blade.php
@@ -21,9 +21,16 @@
           <li><a href="sakusei.blade.php">新規話題を作成</a></li>
         </ul>
     </nav>
-    <input type="text" name="wadai" value="">
-    <input type="submit" value="検索">
-    <p>{{$msg}}</p>
+      <input type="text" name="wadai" value="">
+      <input type="submit" value="検索">
+    <table border="1">
+        <tr><th>話題作成日</th><th>話題名</th><th>書き込み人数</th></tr>
+        <tr>
+              <td>{{$msg}}</td>
+              <td>{{$msg}}</td>
+              <td>{{$msg}}</td>
+        </tr>
+    </table>
   </main>
   <footer>
       <p>upbook1017_kejiban</p>
