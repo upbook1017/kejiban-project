98f05a1aac07b33c13ea6de4b46fe160a1a34e9c
upbook1017 <164146088+upbook1017@users.noreply.github.com>
2024-04-18 01:31:19 +0900

    2024/4/18作業記録
    cssファイルとblade.phpファイルの作成を行なった。次回はコントローラーファイルの作成を行う。

--- index.blade.php
+++ index.blade.php
@@ -21,15 +21,20 @@
           <li><a href="sakusei.blade.php">新規話題を作成</a></li>
         </ul>
     </nav>
-      <input type="text" name="wadai" value="">
+      <input type="text" name="wadai" value="" class="kensaku">
       <input type="submit" value="検索">
+
     <table border="1">
-        <tr><th>話題作成日</th><th>話題名</th><th>書き込み人数</th></tr>
-        <tr>
-              <td>{{$msg}}</td>
-              <td>{{$msg}}</td>
-              <td>{{$msg}}</td>
-        </tr>
+      <tr>
+        <th class="hinichi">話題作成日</th>
+        <th class="naiyou">話題名</th>
+        <th class="kazu">書き込み人数</th>
+      </tr>
+      <tr>
+            <td class="hinichitext">{{$msg}}</td>
+            <td class="naiyoutext">{{$msg}}</td>
+            <td class="kazutext">{{$msg}}</td>
+      </tr>
     </table>
   </main>
   <footer>
