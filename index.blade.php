8b4c249a99b4c134578e4ab864945f93e13005f9
upbook1017 <164146088+upbook1017@users.noreply.github.com>
2024-04-20 17:45:18 +0900

    2024/4/20作業記録
    cssファイルを作成した。(表のデザイン)

--- index.blade.php
+++ index.blade.php
@@ -21,7 +21,8 @@
           <li><a href="sakusei.blade.php">新規話題を作成</a></li>
         </ul>
     </nav>
-      <input type="text" name="wadai" value="" class="kensaku">
+      <input type="text" name="wadai" value="" class="kensaku"
+            placeholder="3~5文字内で「話題名」から検索します。">
       <input type="submit" value="検索">
 
     <table border="1">
