5d1b39e91b6b58ba4ee37ca48d88d9e9a3a265dd
upbook1017 <164146088+upbook1017@users.noreply.github.com>
2024-05-05 19:31:02 +0900

    2024/5/5作業記録
    sakusei.blade.phpを修正

--- sakusei.blade.php
+++ sakusei.blade.php
@@ -1,16 +1,33 @@
 @extends('layouts.kejiban')
 
+<style>
+    table#sakusei1 {padding: 50px; border:#FFFFFF;}
+</style>
+
 @section('main')
   <main>
     <h1>新規話題作成</h1>
-
-    <table border="1">
-      <tr>
-        <th class="hinichi">話題作成日</th>
-      </tr>
-      <tr>
-            <td class="hinichitext">{{$msg}}</td>
-      </tr>
-    </table>
+    <nav>
+        <ul>
+          <li><a href="home">一覧に戻る</a></li>
+        </ul>
+    </nav>
+<table id="sakusei1">
+@csrf
+  <tr>
+    <th>ニックネーム：</th>
+      <td>
+      <input type="text" name="wadai" value="" class=""
+            placeholder="10文字以内">
+      </td>
+  </tr>
+  <tr>
+    <th>話題名：</th>
+      <td>
+      <input type="text" name="wadai" value="" class=""
+            placeholder="15文字以内">
+      </td>
+  </tr>
+</table>
   </main>
 @endsection
